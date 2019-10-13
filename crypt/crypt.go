81808e78b2a7880d77da190627103d7f6d97e50e
Michał Matysek <hello@michalmatysek.com>
2019-10-13 05:32:44 +0200

    Add Crypt class

---
+++ crypt/crypt.go
@@ -0,0 +1,15 @@
+package crypt
+
+import (
+	"bytes"
+	"log"
+	"strconv"
+)
+
+func Crypt(src []byte, decrypt bool) string {
+	var r = bytes.Reader{src}
+
+	log.Fatal(r)
+
+	return "asd"
+}
--- main.go
+++ main.go
@@ -1,4 +1,40 @@
 package main
 
+import (
+	"fmt"
+	"github.com/matysekmichal/mpc.crypt/crypt"
+)
+
 func main() {
+	var (
+		operation = "S"
+		str string
+		decrypt = false
+	)
+
+	fmt.Println("Program pozwalający na szyfrowanie i deszyfrowanie bloków 8-bitowych.")
+	fmt.Print("Chcesz (S)zyfrować, czy (D)eszyfrować (DOMYŚLNIE S): ")
+	var _, err = fmt.Scan(&operation)
+
+	if err != nil || operation != "S" && operation != "D" {
+		fmt.Println(err)
+		panic("Wprowadzono nieprawidłową wartość.")
+	}
+
+	if operation == "D" {
+		decrypt = true
+	}
+
+	fmt.Print("Wprowadź klucz 8-bitowy: ")
+	_, err = fmt.Scan(&str)
+
+	if err != nil || len(str) != 8 {
+		panic("Wprowadzony klucz jest nieprawidłowy.")
+	}
+
+
+
+	result := crypt.Crypt(str, decrypt)
+
+	fmt.Printf("%b", result)
 }
