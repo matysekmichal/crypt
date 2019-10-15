9faef128b4d4ef67709a04a6165f90c3d0545ebc
Michał Matysek <hello@michalmatysek.com>
2019-10-15 22:38:10 +0200

    Encrypt

--- crypt/crypt.go
+++ crypt/crypt.go
@@ -1,15 +1,53 @@
 package crypt
 
 import (
-	"bytes"
 	"log"
-	"strconv"
+	"math/bits"
 )
 
-func Crypt(src []byte, decrypt bool) string {
-	var r = bytes.Reader{src}
+func Crypt(src uint8, secretKey uint8, decrypt bool) uint8 {
+	left, right := src>>4, src&0xF
 
-	log.Fatal(r)
+	log.Printf("FF: %04b %04b", left, right)
 
-	return "asd"
+	for i := 0; i < 8; i++ {
+		left ^= SBlock(i, secretKey, decrypt)
+		left, right = right, left
+	}
+
+	return left<<4 | right
+}
+
+func SBlock(i int, secret uint8, decrypt bool) uint8 {
+	key, r := key(i, secret, decrypt)
+
+	h1 := 1 ^ (r>>1)&1 ^ (r>>3)&1&(r>>2)&1 ^ (r>>2)&1&(r>>1)&1&r&1 ^ (r>>3)&1&(r>>2)&1&(r>>1)&1&r&1 ^ (key>>3)&1
+	h2 := 1 ^ (r>>3)&1&r&1 ^ (r>>3)&1&(r>>1)&1&r&1 ^ (r>>3)&1&(r>>2)&1&(r>>1)&1&r&1 ^ (key>>2)&1
+	h3 := (r>>2)&1 ^ (r>>3)&1&r&1 ^ (r>>3)&1&(r>>1)&1&r&1 ^ (r>>3)&1&(r>>2)&1&(r>>1)&1&r&1 ^ (key>>1)&1
+	h4 := (r>>3)&1&r&1 ^ (r>>2)&1&(r>>1)&1 ^ (r>>3)&1&(r>>2)&1&r&1 ^ (r>>3)&1&(r>>1)&1&r&1 ^ (r>>3)&1&(r>>2)&1&(r>>1)&1&r&1 ^ key&1
+
+	return h1<<3 | h2<<2 | h3<<1 | h4
+}
+
+func key(i int, secret uint8, decrypt bool) (key uint8, rightKey uint8) {
+	var (
+		l uint8
+		r uint8
+	)
+
+	l, r = secret>>4, secret&0xF
+
+	if i < 7 {
+		l, r = RotateLeft4(l, i), RotateLeft4(r, i)
+
+		secret = bits.RotateLeft8(l<<4|r, 1)
+	}
+
+	return secret, r
+}
+
+func RotateLeft4(x uint8, k int) uint8 {
+	const n = 4
+	s := uint(k) & (n - 1)
+	return (x<<s | x>>(n-s)) & 0xF
 }
--- main.go
+++ main.go
@@ -3,15 +3,43 @@ package main
 import (
 	"fmt"
 	"github.com/matysekmichal/mpc.crypt/crypt"
+	"strconv"
+)
+
+var (
+	operation = "S"
+	str       string
+	key       string
+	decrypt   = false
 )
 
 func main() {
-	var (
-		operation = "S"
-		str string
-		decrypt = false
-	)
+	//dataProvider()
 
+	str = "00000000"
+	key = "11100000"
+
+	//str = "01111000"
+	//key = "10110001"
+
+	//str = "11111010"
+	//key = "10101100"
+
+	msg, _ := strconv.ParseInt(str, 2, 16)
+	secretKey, _ := strconv.ParseInt(key, 2, 16)
+
+	a := crypt.Crypt(uint8(msg), uint8(secretKey), decrypt)
+
+	fmt.Printf("%08b", a)
+}
+
+func def() {
+	str = "00000000"
+	//str = "01111000"
+	//str = "11111010"
+}
+
+func dataProvider() {
 	fmt.Println("Program pozwalający na szyfrowanie i deszyfrowanie bloków 8-bitowych.")
 	fmt.Print("Chcesz (S)zyfrować, czy (D)eszyfrować (DOMYŚLNIE S): ")
 	var _, err = fmt.Scan(&operation)
@@ -25,16 +53,17 @@ func main() {
 		decrypt = true
 	}
 
-	fmt.Print("Wprowadź klucz 8-bitowy: ")
+	fmt.Print("Wprowadź wiadomość 8-bitową: ")
 	_, err = fmt.Scan(&str)
 
 	if err != nil || len(str) != 8 {
-		panic("Wprowadzony klucz jest nieprawidłowy.")
+		panic("Wprowadzony wiadomość jest nieprawidłowa.")
 	}
 
+	fmt.Print("Wprowadź klucz szyfrujący 8-bitowy: ")
+	_, err = fmt.Scan(&key)
 
-
-	result := crypt.Crypt(str, decrypt)
-
-	fmt.Printf("%b", result)
+	if err != nil || len(key) != 8 {
+		panic("Wprowadzony klucz szyfrujący jest nieprawidłowy.")
+	}
 }
